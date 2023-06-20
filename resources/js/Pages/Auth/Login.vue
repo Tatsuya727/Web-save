802566b451bdeb3c110f95f1abdcf0ab8f4eb78a
tatsuya <babatsuya@icloud.com>
2023-06-20 12:33:30 +0900

    ログインページ　レイアウト修正

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -37,26 +37,53 @@ const submit = () => {
             <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
                 {{ status }}
             </div>
-            <form @submit.prevent="submit">
+            <form @submit.prevent="submit" class="m-5">
                 <div>
-                    <InputLabel for="email" value="Email" />
-                    <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus autocomplete="username" />
+                    <InputLabel for="email" value="メールアドレス" />
+                    <TextInput
+                        id="email"
+                        type="email"
+                        class="mt-1 block w-full"
+                        v-model="form.email"
+                        required
+                        autofocus
+                        autocomplete="username"
+                    />
                     <InputError class="mt-2" :message="form.errors.email" />
                 </div>
                 <div class="mt-4">
-                    <InputLabel for="password" value="Password" />
-                    <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
+                    <InputLabel for="password" value="パスワード" />
+                    <TextInput
+                        id="password"
+                        type="password"
+                        class="mt-1 block w-full"
+                        v-model="form.password"
+                        required
+                        autocomplete="current-password"
+                    />
                     <InputError class="mt-2" :message="form.errors.password" />
                 </div>
                 <div class="block mt-4">
                     <label class="flex items-center">
                         <Checkbox name="remember" v-model:checked="form.remember" />
-                        <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                        <span class="ml-2 text-sm text-gray-600">ログイン状態を保存する</span>
                     </label>
                 </div>
                 <div class="flex items-center justify-end mt-4">
-                    <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"> Forgot your password? </Link>
-                    <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing"> Log in </PrimaryButton>
+                    <Link
+                        v-if="canResetPassword"
+                        :href="route('password.request')"
+                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        パスワードをお忘れですか?
+                    </Link>
+                    <PrimaryButton
+                        class="ml-4"
+                        :class="{ 'opacity-25': form.processing }"
+                        :disabled="form.processing"
+                    >
+                        ログイン
+                    </PrimaryButton>
                 </div>
             </form>
         </GuestLayout>
