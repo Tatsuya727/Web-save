8520c6bc6287a1387c37131caab2d4d6c6db9554
tatsuya <babatsuya@icloud.com>
2023-06-23 15:07:16 +0900

    レスポンシブ対応

--- resources/js/Pages/Urls/Index.vue
+++ resources/js/Pages/Urls/Index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed, reactive } from 'vue';
+import { ref, computed, reactive, onMounted, onUnmounted } from 'vue';
 import Pagination from '@/Components/Pagination.vue';
 import { Inertia } from '@inertiajs/inertia';
 import { Link } from '@inertiajs/inertia-vue3';
@@ -7,6 +7,21 @@ import { Link } from '@inertiajs/inertia-vue3';
 // definePropsを使用してpropsを定義
 const props = defineProps({
     urls: Object,
+});
+
+const isMobile = ref(false);
+
+const updateIsMobile = () => {
+    isMobile.value = window.innerWidth < 768;
+};
+
+onMounted(() => {
+    window.addEventListener('resize', updateIsMobile);
+    updateIsMobile();
+});
+
+onUnmounted(() => {
+    window.removeEventListener('resize', updateIsMobile);
 });
 
 const dialog = ref(false);
@@ -101,31 +116,104 @@ const drawer = ref(null);
         </v-navigation-drawer>
 
         <v-main>
-            <v-app-bar :elevation="2">
-                <div class="ml-10 text-center">
-                    <v-btn href="/urls" class="mr-10" variant="text"> TOP </v-btn>
-                    <input label="Search" v-model="search" class="w-full px-3 py-2 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 border border-indigo-600" placeholder="Search..." />
-                    <button class="bg-blue-300 py-2 px-2 border border-indigo-600" @click="searchUrls">検索</button>
-                    <v-btn class="ml-10" color="primary" variant="outlined">
-                        URLを登録
+            <div v-if="isMobile">
+                <v-app-bar :elevation="2">
+                    <div class="ml-2 text-center">
+                        <v-btn href="/urls" class="mr-2" variant="text"> TOP </v-btn>
 
-                        <v-dialog v-model="dialog" activator="parent" width="auto">
-                            <v-sheet width="800" class="mx-10">
-                                <v-form @submit.prevent="storeUrl">
-                                    <v-text-field v-model="form.url" :rules="rules" label="URL" :error-messages="form.errors.url"></v-text-field>
-                                    <v-btn type="submit" block class="bg-green">登録</v-btn>
-                                    <!-- <v-autocomplete clearable chips label="タグ" :items="search_items" multiple></v-autocomplete> -->
-                                </v-form>
-                            </v-sheet>
-                        </v-dialog>
-                    </v-btn>
-                </div>
-                <template v-slot:append>
-                    <Link href="/logout" method="post" as="button" class="ml-10 bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> ログアウト </Link>
-                </template>
-            </v-app-bar>
+                        <v-btn color="primary" variant="outlined">
+                            URLを登録
+                            <v-dialog v-model="dialog" activator="parent" width="auto">
+                                <v-sheet width="350">
+                                    <v-form @submit.prevent="storeUrl">
+                                        <v-text-field
+                                            v-model="form.url"
+                                            :rules="rules"
+                                            label="URL"
+                                            :error-messages="form.errors.url"
+                                        ></v-text-field>
+                                        <v-btn type="submit" block class="bg-green">登録</v-btn>
+                                        <!-- <v-autocomplete clearable chips label="タグ" :items="search_items" multiple></v-autocomplete> -->
+                                    </v-form>
+                                </v-sheet>
+                            </v-dialog>
+                        </v-btn>
+                    </div>
+                    <template v-slot:append>
+                        <Link
+                            href="/logout"
+                            method="post"
+                            as="button"
+                            class="bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        >
+                            ログアウト
+                        </Link>
+                    </template>
+                </v-app-bar>
+            </div>
+            <!-- レスポンシブ対応（モバイル） -->
+            <div v-else>
+                <v-app-bar :elevation="2">
+                    <div class="ml-10 text-center">
+                        <v-btn href="/urls" class="mr-10" variant="text"> TOP </v-btn>
+                        <input
+                            label="Search"
+                            v-model="search"
+                            class="w-full px-3 py-2 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 border border-indigo-600"
+                            placeholder="Search..."
+                        />
+                        <button
+                            class="bg-blue-300 py-2 px-2 border border-indigo-600"
+                            @click="searchUrls"
+                        >
+                            検索
+                        </button>
+                        <v-btn class="ml-10" color="primary" variant="outlined">
+                            URLを登録
+                            <v-dialog v-model="dialog" activator="parent" width="auto">
+                                <v-sheet width="800" class="mx-10">
+                                    <v-form @submit.prevent="storeUrl">
+                                        <v-text-field
+                                            v-model="form.url"
+                                            :rules="rules"
+                                            label="URL"
+                                            :error-messages="form.errors.url"
+                                        ></v-text-field>
+                                        <v-btn type="submit" block class="bg-green">登録</v-btn>
+                                        <!-- <v-autocomplete clearable chips label="タグ" :items="search_items" multiple></v-autocomplete> -->
+                                    </v-form>
+                                </v-sheet>
+                            </v-dialog>
+                        </v-btn>
+                    </div>
+                    <template v-slot:append>
+                        <Link
+                            href="/logout"
+                            method="post"
+                            as="button"
+                            class="ml-10 bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        >
+                            ログアウト
+                        </Link>
+                    </template>
+                </v-app-bar>
+            </div>
             <v-container class="py-8 px-6" fluid>
                 <v-row>
+                    <div v-if="isMobile" class="ml-9">
+                        <input
+                            label="Search"
+                            v-model="search"
+                            class="px-3 py-2 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 border border-indigo-600"
+                            placeholder="Search..."
+                        />
+                        <button
+                            class="bg-blue-300 py-2 px-2 border border-indigo-600"
+                            @click="searchUrls"
+                        >
+                            検索
+                        </button>
+                    </div>
                     <v-col v-for="(urlsByDate, date) in urlsGroupedByDate" :key="date" cols="12">
                         <v-card>
                             <v-list lines="two">
@@ -133,7 +221,14 @@ const drawer = ref(null);
                                 <template v-for="url in urlsByDate" :key="url.id">
                                     <v-list-item>
                                         <template v-slot:prepend>
-                                            <img v-if="url.favicon" :src="url.favicon" alt="Favicon" width="42" height="42" class="mr-5" />
+                                            <img
+                                                v-if="url.favicon"
+                                                :src="url.favicon"
+                                                alt="Favicon"
+                                                width="42"
+                                                height="42"
+                                                class="mr-5"
+                                            />
                                             <v-avatar v-else color="grey-darken-1"></v-avatar>
                                         </template>
 
